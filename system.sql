5bc41994ff7c0e5af8635a6c2856559cbd505f44
jun960303 <rhkddnjs7878@naver.com>
2025-10-24 12:09:05 +0900

    SQL파일작성

---
+++ system.sql
@@ -0,0 +1,9 @@
+-- C##  을 사용안하도록 설정 
+-- 오라클버전이 업데이트되면서 사용자 설정할 때 아이디 앞에 C##을 붙이도록 설정되어 있음 
+-- hr 사용자 생성 => c##hr
+ALTER SESSION SET "_oracle_script"=TRUE;
+
+@C:\Users\rhkdd\Downloads\db-sample-schemas-main\db-sample-schemas-main\human_resources\hr_install.sql
+@C:\app\rhkdd\product\21c\dbhomeXE\rdbms\admin\scott.sql
+
+sys AS ysdba
